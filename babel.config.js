54735f6b59cc5b2de2d3c036e04cad24d0362b46
Eudes Johnson <eudesjohn650@gmail.com>
2026-07-06 11:43:02 +0100

    Fix: add worklets babel plugin + native build properties for Android

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,9 @@ module.exports = function (api) {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
     ],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-worklets/plugin',
+      'react-native-reanimated/plugin',
+    ],
   };
 };
